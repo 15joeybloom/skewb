d9576b9cac21e76c72add991c6e147aee79624c3
Joey Bloom <15joeybloom@gmail.com>
2018-07-28 13:34:53 -0700

    Change internal representation of Skewb to use indices
    ...instead of Corners and CornerPieces. This should reduce the memory footprint
    of a Skewb.

--- src/skewb.rs
+++ src/skewb.rs
@@ -86,8 +86,8 @@ impl Center {
 
 #[derive(Debug)]
 pub struct Skewb {
-    corner_pieces: HashMap<Corner, CornerPiece>,
-    corner_orientations: HashMap<Corner, Orientation>,
+    corner_pieces: [usize; 8],
+    corner_orientations: [Orientation; 8],
     center_pieces: HashMap<Center, Color>,
 }
 
@@ -102,26 +102,8 @@ macro_rules! hashmap {
 impl Skewb {
     pub fn new() -> Skewb {
         Skewb {
-            corner_pieces: hashmap![
-                (0, 0, 0) => CornerPiece(Color::Y, Color::O, Color::G),
-                (0, 0, 1) => CornerPiece(Color::Y, Color::O, Color::B),
-                (0, 1, 1) => CornerPiece(Color::Y, Color::R, Color::B),
-                (0, 1, 0) => CornerPiece(Color::Y, Color::R, Color::G),
-                (1, 0, 0) => CornerPiece(Color::W, Color::O, Color::G),
-                (1, 0, 1) => CornerPiece(Color::W, Color::O, Color::B),
-                (1, 1, 1) => CornerPiece(Color::W, Color::R, Color::B),
-                (1, 1, 0) => CornerPiece(Color::W, Color::R, Color::G),
-            ],
-            corner_orientations: hashmap![
-                (0, 0, 0) => Orientation::UD,
-                (0, 0, 1) => Orientation::UD,
-                (0, 1, 1) => Orientation::UD,
-                (0, 1, 0) => Orientation::UD,
-                (1, 0, 0) => Orientation::UD,
-                (1, 0, 1) => Orientation::UD,
-                (1, 1, 1) => Orientation::UD,
-                (1, 1, 0) => Orientation::UD,
-            ],
+            corner_pieces: [0, 1, 2, 3, 4, 5, 6, 7],
+            corner_orientations: [Orientation::UD; 8],
             center_pieces: hashmap![
                 Center::U => Color::Y,
                 Center::F => Color::B,
@@ -133,9 +115,50 @@ impl Skewb {
         }
     }
 
-    pub fn get_corner_piece(&self, c: &Corner) -> CornerPiece { self.corner_pieces[c] }
-    pub fn get_corner_orientation(&self, c: &Corner) -> Orientation { self.corner_orientations[c] }
+    fn corner_to_i(c: &Corner) -> usize {
+        match *c {
+            (0, 0, 0) => 0,
+            (0, 0, 1) => 1,
+            (0, 1, 1) => 2,
+            (0, 1, 0) => 3,
+            (1, 0, 0) => 4,
+            (1, 0, 1) => 5,
+            (1, 1, 1) => 6,
+            (1, 1, 0) => 7,
+            x => panic!(format!("{:?} not a corner", x)),
+        }
+    }
+    fn i_to_corner_piece(i: usize) -> CornerPiece {
+        match i {
+            0 => CornerPiece(Color::Y, Color::O, Color::G),
+            1 => CornerPiece(Color::Y, Color::O, Color::B),
+            2 => CornerPiece(Color::Y, Color::R, Color::B),
+            3 => CornerPiece(Color::Y, Color::R, Color::G),
+            4 => CornerPiece(Color::W, Color::O, Color::G),
+            5 => CornerPiece(Color::W, Color::O, Color::B),
+            6 => CornerPiece(Color::W, Color::R, Color::B),
+            7 => CornerPiece(Color::W, Color::R, Color::G),
+            x => panic!(format!("{:?} not a corner piece index", x)),
+        }
+    }
+
+    pub fn get_corner_piece(&self, c: &Corner) -> CornerPiece {
+        Self::i_to_corner_piece(self.corner_pieces[Self::corner_to_i(c)])
+    }
+    pub fn get_corner_orientation(&self, c: &Corner) -> Orientation {
+        self.corner_orientations[Self::corner_to_i(c)]
+    }
     pub fn get_center_piece(&self, c: &Center) -> Color { self.center_pieces[c] }
+
+    fn rotate_elements<V>(array: &mut [V], keys: &Vec<usize>) {
+        if keys.len() <= 1 {
+            return;
+        }
+
+        for i in 0..keys.len() - 1 {
+            array.swap(keys[i], keys[i + 1]);
+        }
+    }
 
     fn rotate_values<K, V>(h: &mut HashMap<K, V>, keys: &[K])
     where K: Eq + Hash + Copy {
@@ -145,8 +168,6 @@ impl Skewb {
 
         let temp = h.remove(keys.first().unwrap()).unwrap();
         for i in 0..keys.len() - 1 {
-            //mem::swap(h.get_mut(&keys[i]).unwrap(), h.get_mut(&keys[i+1]).unwrap());
-            //*h.get_mut(&keys[i]).unwrap() = *h.get(&keys[i+1]).unwrap();
             let x = h.remove(&keys[i + 1]).unwrap();
             h.insert(keys[i], x);
         }
@@ -158,15 +179,20 @@ impl Skewb {
             (c.0, c.1, 1 - c.2),
             (c.0, 1 - c.1, c.2),
             (1 - c.0, c.1, c.2),
-        ];
+        ].into_iter()
+            .map(|x| Self::corner_to_i(&x))
+            .collect();
 
-        *self.corner_orientations.get_mut(&c).unwrap() += Orientation::LR;
+        *self
+            .corner_orientations
+            .get_mut(Self::corner_to_i(&c))
+            .unwrap() += Orientation::LR;
 
         Self::rotate_values(&mut self.center_pieces, &Center::centers_for_corner(&c));
 
-        Self::rotate_values(&mut self.corner_pieces, &corners);
-        Self::rotate_values(&mut self.corner_orientations, &corners);
-        for c in (&corners).iter() {
+        Self::rotate_elements(&mut self.corner_pieces, &corners);
+        Self::rotate_elements(&mut self.corner_orientations, &corners);
+        for &c in corners.iter() {
             *self.corner_orientations.get_mut(c).unwrap() += Orientation::LR;
         }
     }
